0f4371b2cf58ad1b9a7d6c66caec7157fbb4ee14
danran66 <daniel2003scc@gmail.com>
2023-06-07 17:57:01 -0300

    adegadosabor

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-	define('HOST', 'localhost');
-	define('USER', 'root');
-	define('PASS', '');
-	define('BASE', 'projeto');
+	define('HOST', 'us-cdbr-east-06.cleardb.net');
+	define('USER', 'bb5982cd95c605');
+	define('PASS', '63ee175c');
+	define('BASE', 'heroku_6b78a9a02492159');
 
 	$conn = new MySQLi(HOST,USER,PASS,BASE);
 	
